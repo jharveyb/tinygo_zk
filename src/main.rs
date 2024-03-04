88c43a7a25fad2f9d5095e72dd83e8cf061834ad
Jonathan Harvey-Buschel <j.s.harbus@gmail.com>
2024-03-04 14:16:12 -0500

    move closer to no_std

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+extern crate alloc;
+
+use alloc::str;
 use anyhow::{Error, Ok};
 use wasmi::{Linker, Value};
 
-fn main() -> anyhow::Result<()> {
+fn main() {
     // have to load this at build time to go no_std
     let go_wasm_path = "megaopt_go.wasm";
-    let wasm_bytes = std::fs::read(go_wasm_path)?;
+    let wasm_bytes = std::fs::read(go_wasm_path).expect("");
 
-    // We have to map the error as we're using wasmi in no_std mode.
+    // We have to map wasmi errors as we're using wasmi in no_std mode.
     let engine = wasmi::Engine::default();
-    let module = wasmi::Module::new(&engine, &wasm_bytes[..]).map_err(Error::msg)?;
+    let module = wasmi::Module::new(&engine, &wasm_bytes[..]).expect("");
 
     // Exposing host function to WASM module
     /*
@@ -28,12 +31,12 @@ fn main() -> anyhow::Result<()> {
     let mut store = wasmi::Store::new(&engine, ());
     let instance = linker
         .instantiate(&mut store, &module)
-        .map_err(Error::msg)?
+        .expect("")
         .start(&mut store)
-        .map_err(Error::msg)?;
+        .expect("");
     let guest_memory = instance
         .get_memory(&store, "memory")
-        .ok_or(Error::msg("failed to get guest memory"))?;
+        .ok_or(Error::msg("failed to get guest memory")).expect("");
 
     println!(
         "Guest memory state: {:?}",
@@ -41,67 +44,68 @@ fn main() -> anyhow::Result<()> {
     );
 
     const SAMPLE_SIZE: usize = 2 << 9;
+    const GUEST_MEM_OFFSET: usize = 2 << 15;
     let read_guest_mem = |s: &wasmi::Store<()>| -> anyhow::Result<()> {
         let mut read_buf: [u8; SAMPLE_SIZE] = [0; SAMPLE_SIZE];
         guest_memory
-            .read(s, 2 << 15, &mut read_buf)
+            .read(s, GUEST_MEM_OFFSET, &mut read_buf)
             .map_err(Error::msg)?;
-        println!("Guest memory Page 2: {:?}", read_buf);
+        println!("Guest memory Page 2: {:02X?}", read_buf);
         Ok(())
     };
-    read_guest_mem(&store)?;
+    read_guest_mem(&store).expect("");
 
     // Register guest funcs
     let extend = instance
         .get_typed_func::<(i32, i32), u64>(&store, "extend")
-        .map_err(Error::msg)?;
+        .expect("register extend guest func");
     let ahoy = instance
         .get_typed_func::<(i32, i32), u64>(&store, "ahoy")
-        .map_err(Error::msg)?;
+        .expect("register ahoy guest func");
     let malloc = instance
         .get_typed_func::<i32, i32>(&store, "_malloc")
-        .map_err(Error::msg)?;
+        .expect("register _malloc guest func");
 
     // Ask guest to make a buffer for host to write to; we have to free later
     let greet_string = "Honig";
     let greet_bytes = greet_string.as_bytes();
     let greet_size = greet_bytes.len() as i32;
 
-    let greet_ptr = malloc.call(&mut store, greet_size).map_err(Error::msg)?;
+    let greet_ptr = malloc.call(&mut store, greet_size).expect("call malloc");
     let greet_ptr_host = greet_ptr as usize;
 
-    println!("Guest ptr: {:X}", greet_ptr);
+    println!("Guest ptr: 0x{:02X}", greet_ptr - GUEST_MEM_OFFSET as i32);
 
     // Write to guest memory directly, yolo
     guest_memory
         .write(&mut store, greet_ptr_host, greet_bytes)
-        .map_err(Error::msg)?;
-    read_guest_mem(&store)?;
+        .expect("write str");
+    read_guest_mem(&store).expect("");
 
     // quick maths
-    let maths = extend.call(&mut store, (1, 2)).map_err(Error::msg)?;
+    let maths = extend.call(&mut store, (1, 2)).expect("call extend");
 
     println!("Quick maths: {:?}", maths);
 
     // call a guest func on the written string
     let ahoy_resp = ahoy
         .call(&mut store, (greet_ptr, greet_size))
-        .map_err(Error::msg)?;
-    read_guest_mem(&store)?;
+        .expect("call ahoy");
+    read_guest_mem(&store).expect("");
 
+    // decode the guest memory location of the func output
     let ahoy_ptr_guest = (ahoy_resp >> 32) as u32;
     let ahoy_size_guest = (ahoy_resp & 0x000000000ffffffff) as u32;
-    eprintln!(
-        "Guest result ptr: {:X}, size: {:X}",
-        ahoy_ptr_guest, ahoy_size_guest
+    println!(
+        "Guest result ptr: 0x{:X}, size: {}",
+        ahoy_ptr_guest - GUEST_MEM_OFFSET as u32, ahoy_size_guest
     );
 
     // read output of guest func from guest memory
     let mut ahoy_bytes = vec![0; ahoy_size_guest as usize];
     guest_memory
-        .read(&store, ahoy_ptr_guest.try_into()?, &mut ahoy_bytes[..])
-        .map_err(Error::msg)?;
-    eprintln!("Guest output: {:?}", std::str::from_utf8(&ahoy_bytes)?);
-
-    anyhow::Ok(())
+        .read(&store, ahoy_ptr_guest as usize, &mut ahoy_bytes[..])
+        .expect("read str");
+    let guest_output = str::from_utf8(&ahoy_bytes).expect("");
+    println!("Guest output: {:?}", guest_output);
 }
